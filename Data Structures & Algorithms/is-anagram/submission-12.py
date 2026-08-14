e2950341523173a5f2e715732a1be503f2ae2fac
Zhuolin Li <157834503+zhu0lin@users.noreply.github.com>
2026-08-14 16:21:26 -0400

    Add: is-anagram - submission-12

---
+++ Data Structures & Algorithms/is-anagram/submission-12.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        freq = [0] * 26
+        for i in range(len(s)):
+            freq[ord(s[i]) - ord('a')] += 1
+            freq[ord(t[i]) - ord('a')] -= 1
+
+        for letter in freq:
+            if letter != 0:
+                return False
+
+        return True
